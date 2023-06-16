c2fafa82e6b1dfd980f326918dd23f68d3cd4967
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-06-16 05:30:13 -0500

    Add MenuItem component to UserMenu component

--- src/components/Navbar/UserMenu/UserMenu.tsx
+++ src/components/Navbar/UserMenu/UserMenu.tsx
@@ -3,6 +3,7 @@
 import { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { Avatar } from '@components/Avatar';
+import { MenuItem } from '@components/Navbar/MenuItem';
 
 export const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -30,6 +31,16 @@ export const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem onClick={() => {}} label='Login' />
+              <MenuItem onClick={() => {}} label='Sign up' />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
