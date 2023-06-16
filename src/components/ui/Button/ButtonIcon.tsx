fd4d27266e6e65595bbc269831819a570f304350
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-06-16 08:49:43 -0500

    Create ButtonIcon component for Button component

---
+++ src/components/ui/Button/ButtonIcon.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import type { IconType } from 'react-icons';
+
+interface ButtonIconProps {
+  icon: IconType;
+}
+
+export const ButtonIcon: React.FC<ButtonIconProps> = ({ icon: Icon }) => {
+  return <Icon className='absolute left-4 top-3' />;
+};
