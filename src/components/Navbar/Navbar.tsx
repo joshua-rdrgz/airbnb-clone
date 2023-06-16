4a8a2f3120cde82ad3e1fb37ce205c75f11df2fc
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-06-16 05:03:28 -0500

    Use Logo, Search, UserMenu components in Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,19 @@
+import { Container } from '@components/Container';
+import { Logo } from '@components/Navbar/Logo';
+import { Search } from '@components/Navbar/Search';
+import { UserMenu } from '@components/Navbar/UserMenu';
+
 export const Navbar = () => {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
-        
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
       </div>
     </div>
   );
