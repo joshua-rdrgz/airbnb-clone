0e5e624456c8454481c99c61b0c98e0136d7821d
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-06-24 12:05:21 -0500

    Pull in currentUser and pass to Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
 import { Navbar } from '@ui/Navbar';
+import { getCurrentUser } from '../actions/getCurrentUser';
 import { ToasterProvider } from '@providers/ToasterProvider';
 
 const font = Nunito({ subsets: ['latin'] });
@@ -10,16 +11,18 @@ export const metadata = {
   description: 'This is a clone of the Airbnb website.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang='en'>
       <body className={font.className}>
         <ToasterProvider />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
