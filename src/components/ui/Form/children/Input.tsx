5ce1f3abfd88c6c62d0d938a19e294fa1ac19ea1
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-07-02 00:47:15 -0500

    Refactor Input - malleable structure

--- src/components/ui/Form/children/Input.tsx
+++ src/components/ui/Form/children/Input.tsx
@@ -1,81 +1,33 @@
-import { useContext } from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { useContext, type InputHTMLAttributes } from 'react';
+import { FormContext, FormContextProps } from '../Form';
+import { useFormContext } from 'react-hook-form';
 import { BiDollar } from 'react-icons/bi';
-import { FormContext, type FormContextProps } from '../Form';
 
-/**
- * TYPES
- */
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  styles?: string | ((props: { [key: string]: any }) => string);
 
-interface InputProps
-  extends VariantProps<typeof inputStyles & typeof labelStyles> {
-  id: string;
-  label: string;
-  type?: string;
-  required?: boolean;
+  /** If currency is involved with input / label duo */
   formatPrice?: boolean;
+
+  /** Natvie to HTML input element, but need to be required */
+  id: string;
 }
 
-/**
- * STYLES
- */
-const inputStyles = cva(
-  'peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed',
-  {
-    variants: {
-      formatPrice: {
-        true: 'pl-9',
-        false: 'pl-4',
-      },
-      hasErrors: {
-        true: 'border-rose-500 focus:border-rose-500',
-        false: 'border-neutral-300 focus:border-black',
-      },
-    },
-    defaultVariants: {
-      formatPrice: false,
-      hasErrors: false,
-    },
-  }
-);
-
-const labelStyles = cva(
-  'absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4',
-  {
-    variants: {
-      formatPrice: {
-        true: 'left-9',
-        false: 'left-4',
-      },
-      hasErrors: {
-        true: 'text-rose-500',
-        false: 'text-zinc-400',
-      },
-    },
-    defaultVariants: {
-      formatPrice: false,
-      hasErrors: false,
-    },
-  }
-);
-
-/**
- * COMPONENTS
- */
-
 export const Input: React.FC<InputProps> = ({
-  id,
-  label,
-  type,
-  required,
+  styles,
   formatPrice,
+  id,
+  required,
+  name,
+  ...restProps
 }) => {
-  const { isLoading, register, errors } = useContext(
-    FormContext
-  ) as FormContextProps;
+  const { isLoading } = useContext(FormContext) as FormContextProps;
+  const methods = useFormContext();
+
+  const stylesIsCva = typeof styles !== 'string';
 
   return (
-    <div className='w-full relative'>
+    <>
       {formatPrice && (
         <BiDollar
           size={24}
@@ -83,24 +35,19 @@ export const Input: React.FC<InputProps> = ({
         />
       )}
       <input
+        {...methods.register(name ? name : id, { required })}
         id={id}
         disabled={isLoading}
-        {...register(id, { required })}
-        placeholder=' '
-        type={type}
-        className={inputStyles({
-          formatPrice,
-          hasErrors: errors?.[id] && true,
-        })}
+        className={
+          stylesIsCva
+            ? styles?.({
+                formatPrice,
+                hasErrors: methods.formState.errors[id] && true,
+              })
+            : styles
+        }
+        {...restProps}
       />
-      <label
-        className={labelStyles({
-          formatPrice,
-          hasErrors: errors?.[id] && true,
-        })}
-      >
-        {label}
-      </label>
-    </div>
+    </>
   );
 };
