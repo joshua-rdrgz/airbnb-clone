8882fe5128679c18b4b16bec9fbc8088eb7fffd0
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-07-02 00:55:55 -0500

    Update AirbnbYourHome usage in UserMenu

--- src/components/ui/Navbar/UserMenu/UserMenu.tsx
+++ src/components/ui/Navbar/UserMenu/UserMenu.tsx
@@ -19,16 +19,7 @@ export const UserMenu = () => {
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
-        {currentUser ? (
-          <div
-            onClick={() => {}}
-            className='hidden text-sm font-semibold py-3 px-4 rounded-full transition cursor-pointer md:block hover:bg-neutral-100'
-          >
-            Airbnb your home
-          </div>
-        ) : (
-          <AirbnbYourHome />
-        )}
+        <AirbnbYourHome />
         <div
           onClick={toggleOpen}
           className='p-4 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer transition md:py-1 md:px-2 hover:shadow-md'
