3032b2accf6e48baf6e6e534177505689a5e7fe3
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-07-02 00:53:12 -0500

    Refactor Form Usage in RegisterModal

--- src/components/ui/Modal/GuestModal/RegisterModal/RegisterModal.tsx
+++ src/components/ui/Modal/GuestModal/RegisterModal/RegisterModal.tsx
@@ -3,9 +3,50 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { FieldValues } from 'react-hook-form';
+import { cva } from 'class-variance-authority';
 import { Heading } from '@ui/Heading';
 import { Form } from '@ui/Form';
 import { SocialSignUpOrIn } from '@ui/Modal/GuestModal/SocialSignUpOrIn';
+
+const inputStyles = cva(
+  'peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed',
+  {
+    variants: {
+      formatPrice: {
+        true: 'pl-9',
+        false: 'pl-4',
+      },
+      hasErrors: {
+        true: 'border-rose-500 focus:border-rose-500',
+        false: 'border-neutral-300 focus:border-black',
+      },
+    },
+    defaultVariants: {
+      formatPrice: false,
+      hasErrors: false,
+    },
+  }
+);
+
+const labelStyles = cva(
+  'absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4',
+  {
+    variants: {
+      formatPrice: {
+        true: 'left-9',
+        false: 'left-4',
+      },
+      hasErrors: {
+        true: 'text-rose-500',
+        false: 'text-zinc-400',
+      },
+    },
+    defaultVariants: {
+      formatPrice: false,
+      hasErrors: false,
+    },
+  }
+);
 
 interface RegisterModalProps {
   /** closeModal is received from Modal.Window */
@@ -35,17 +76,39 @@ export const RegisterModal: React.FC<RegisterModalProps> = ({
             <Heading.Title>Welcome to Airbnb</Heading.Title>
             <Heading.Subtitle>Create an account!</Heading.Subtitle>
           </Heading>
-          <Form onSubmit={onFormSubmit}>
-            <Form.Input id='email' label='Email' required />
-            <Form.Input id='name' label='Name' required />
-            <Form.Input
-              id='password'
-              label='Password'
-              type='password'
-              required
-            />
+          <Form
+            onSubmit={onFormSubmit}
+            defaultValues={{
+              email: '',
+              name: '',
+              password: '',
+            }}
+          >
+            <div className='w-full relative'>
+              <Form.Input id='email' styles={inputStyles} required />
+              <Form.Label htmlFor='email' styles={labelStyles}>
+                Email
+              </Form.Label>
+            </div>
+            <div className='w-full relative'>
+              <Form.Input id='name' styles={inputStyles} required />
+              <Form.Label htmlFor='name' styles={labelStyles}>
+                Name
+              </Form.Label>
+            </div>
+            <div className='w-full relative'>
+              <Form.Input
+                id='password'
+                type='password'
+                styles={inputStyles}
+                required
+              />
+              <Form.Label htmlFor='password' styles={labelStyles}>
+                Password
+              </Form.Label>
+            </div>
             <Form.Footer>
-              <Form.Submit>Continue</Form.Submit>
+              <Form.Submit>Register</Form.Submit>
             </Form.Footer>
           </Form>
         </div>
