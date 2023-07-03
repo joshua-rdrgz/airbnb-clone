7d33ec97a382a304902d4fe03ee12c1894c6d4ff
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-07-02 21:24:23 -0500

    Fix AirbnbYourHomeForm implementation

--- src/components/ui/Modal/RentModal/RentModal.tsx
+++ src/components/ui/Modal/RentModal/RentModal.tsx
@@ -1,20 +1,13 @@
 'use client';
 
-import { Heading } from '@ui/Heading';
-import { AirbnbYourHomeForm } from './AirbnbYourHomeForm';
+import { AirbnbYourHomeForm } from './AirbnbYourHomeForm/AirbnbYourHomeForm';
 
 export const RentModal = () => {
   return (
     <>
       <div className='relative p-6 flex-auto'>
-        <div className='flex flex-col gap-4'>
-          <Heading>
-            <Heading.Title>
-              Which of these best describes your place?
-            </Heading.Title>
-            <Heading.Subtitle>Pick a category</Heading.Subtitle>
-            <AirbnbYourHomeForm />
-          </Heading>
+        <div className='flex flex-col gap-8'>
+          <AirbnbYourHomeForm />
         </div>
       </div>
     </>
