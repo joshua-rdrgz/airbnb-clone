c6e542b6295e42d17068e24be04bbdbbe6080971
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-09 00:52:20 -0500

    Refactor: use TextInput UI w/DescriptionStep

--- src/components/ui/Modal/RentModal/AirbnbYourHomeForm/steps/DescriptionStep.tsx
+++ src/components/ui/Modal/RentModal/AirbnbYourHomeForm/steps/DescriptionStep.tsx
@@ -1,46 +1,5 @@
-import { cva } from 'class-variance-authority';
 import { Heading } from '@ui/Heading';
-import { Form } from '@ui/Form';
-
-const inputStyles = cva(
-  'peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed',
-  {
-    variants: {
-      formatPrice: {
-        true: 'pl-9',
-        false: 'pl-4',
-      },
-      hasErrors: {
-        true: 'border-rose-500 focus:border-rose-500',
-        false: 'border-neutral-300 focus:border-black',
-      },
-    },
-    defaultVariants: {
-      formatPrice: false,
-      hasErrors: false,
-    },
-  }
-);
-
-const labelStyles = cva(
-  'absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4',
-  {
-    variants: {
-      formatPrice: {
-        true: 'left-9',
-        false: 'left-4',
-      },
-      hasErrors: {
-        true: 'text-rose-500',
-        false: 'text-zinc-400',
-      },
-    },
-    defaultVariants: {
-      formatPrice: false,
-      hasErrors: false,
-    },
-  }
-);
+import { TextInput } from '@ui/TextInput';
 
 export const DescriptionStep = () => {
   return (
@@ -49,19 +8,9 @@ export const DescriptionStep = () => {
         <Heading.Title>How would you describe your place?</Heading.Title>
         <Heading.Subtitle>Short and sweet works best!</Heading.Subtitle>
       </Heading>
-      <div className='w-full relative'>
-        <Form.Input id='title' styles={inputStyles} />
-        <Form.Label styles={labelStyles} htmlFor='title'>
-          Title
-        </Form.Label>
-      </div>
+      <TextInput inputId='title' label='Title' />
       <hr />
-      <div className='w-full relative'>
-        <Form.Input id='description' styles={inputStyles} />
-        <Form.Label styles={labelStyles} htmlFor='description'>
-          Description
-        </Form.Label>
-      </div>
+      <TextInput inputId='description' label='Description' />
     </div>
   );
 };
