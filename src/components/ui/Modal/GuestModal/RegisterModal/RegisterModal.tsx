e92316128cf13e7341d8bc99886cb55f9a17320f
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-09 00:52:59 -0500

    Refactor: use TextInput UI w/RegisterModal

--- src/components/ui/Modal/GuestModal/RegisterModal/RegisterModal.tsx
+++ src/components/ui/Modal/GuestModal/RegisterModal/RegisterModal.tsx
@@ -7,46 +7,7 @@ import { cva } from 'class-variance-authority';
 import { Heading } from '@ui/Heading';
 import { Form } from '@ui/Form';
 import { SocialSignUpOrIn } from '@ui/Modal/GuestModal/SocialSignUpOrIn';
-
-const inputStyles = cva(
-  'peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed',
-  {
-    variants: {
-      formatPrice: {
-        true: 'pl-9',
-        false: 'pl-4',
-      },
-      hasErrors: {
-        true: 'border-rose-500 focus:border-rose-500',
-        false: 'border-neutral-300 focus:border-black',
-      },
-    },
-    defaultVariants: {
-      formatPrice: false,
-      hasErrors: false,
-    },
-  }
-);
-
-const labelStyles = cva(
-  'absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4',
-  {
-    variants: {
-      formatPrice: {
-        true: 'left-9',
-        false: 'left-4',
-      },
-      hasErrors: {
-        true: 'text-rose-500',
-        false: 'text-zinc-400',
-      },
-    },
-    defaultVariants: {
-      formatPrice: false,
-      hasErrors: false,
-    },
-  }
-);
+import { TextInput } from '@ui/TextInput';
 
 interface RegisterModalProps {
   /** closeModal is received from Modal.Window */
@@ -84,29 +45,24 @@ export const RegisterModal: React.FC<RegisterModalProps> = ({
               password: '',
             }}
           >
-            <div className='w-full relative'>
-              <Form.Input id='email' styles={inputStyles} required />
-              <Form.Label htmlFor='email' styles={labelStyles}>
-                Email
-              </Form.Label>
-            </div>
-            <div className='w-full relative'>
-              <Form.Input id='name' styles={inputStyles} required />
-              <Form.Label htmlFor='name' styles={labelStyles}>
-                Name
-              </Form.Label>
-            </div>
-            <div className='w-full relative'>
-              <Form.Input
-                id='password'
-                type='password'
-                styles={inputStyles}
-                required
-              />
-              <Form.Label htmlFor='password' styles={labelStyles}>
-                Password
-              </Form.Label>
-            </div>
+            <TextInput
+              inputId='email'
+              addInputProps={{ required: true }}
+              label='Email'
+            />
+            <TextInput
+              inputId='name'
+              addInputProps={{ required: true }}
+              label='Name'
+            />
+            <TextInput
+              inputId='password'
+              addInputProps={{
+                type: 'password',
+                required: true,
+              }}
+              label='Password'
+            />
             <Form.Footer>
               <Form.Submit>Register</Form.Submit>
             </Form.Footer>
