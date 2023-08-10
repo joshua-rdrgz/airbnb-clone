36ec28b6593c3639d26954c2436ea6292e5ae761
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-10 12:30:41 -0500

    create NotLoggedInModalWindow UI component

---
+++ src/components/ui/Modal/GuestModal/NotLoggedInModalWindow.tsx
@@ -0,0 +1,18 @@
+import { Modal } from '@ui/Modal';
+import { LoginModal } from './LoginModal';
+import { RegisterModal } from './RegisterModal';
+
+export const NotLoggedInModalWindow = () => {
+  return (
+    <>
+      <Modal.Window name='login_modal'>
+        <Modal.Heading>Log in</Modal.Heading>
+        <LoginModal />
+      </Modal.Window>
+      <Modal.Window name='sign_up_modal'>
+        <Modal.Heading>Sign up</Modal.Heading>
+        <RegisterModal />
+      </Modal.Window>
+    </>
+  );
+};
