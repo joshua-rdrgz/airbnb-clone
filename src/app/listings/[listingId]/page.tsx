d86bb6144d27a541362ba9511237b4963ccfdb62
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-10 11:48:40 -0500

    Add reservations

--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -2,6 +2,7 @@ import { getCurrentUser } from '@/actions/getCurrentUser';
 import getListingById from '@/actions/getListingById';
 import { EmptyState } from '@ui/EmptyState';
 import { ListingClient } from './ListingClient';
+import getReservations from '@/actions/getReservations';
 
 interface IParams {
   listingId?: string;
@@ -9,9 +10,16 @@ interface IParams {
 
 export default async function ListingPage({ params }: { params: IParams }) {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) return <EmptyState />;
 
-  return <ListingClient listing={listing} currentUser={currentUser} />;
+  return (
+    <ListingClient
+      listing={listing}
+      currentUser={currentUser}
+      reservations={reservations}
+    />
+  );
 }
