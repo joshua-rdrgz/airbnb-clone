feafa438cf99b63e776cb359221eb2691818bc87
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-15 18:38:23 -0500

    Add SearchModal to Searchbar

--- src/components/ui/Navbar/Search/Search.tsx
+++ src/components/ui/Navbar/Search/Search.tsx
@@ -1,22 +1,33 @@
 'use client';
 
 import { BiSearch } from 'react-icons/bi';
+import { Modal } from '@ui/Modal';
+import { SearchModal } from './SearchModal';
 
 export const Search = () => {
   return (
-    <div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
-      <div className='flex flex-row items-center justify-between'>
-        <div className='text-sm font-semibold px-6'>Anywhere</div>
-        <div className='hidden text-sm font-semibold px-6 border-x-[1px] flex-1 text-center sm:block'>
-          Any week
-        </div>
-        <div className='text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3'>
-          <div className='hidden sm:block'>Add Guests</div>
-          <div className='p-2 bg-rose-500 rounded-full text-white'>
-            <BiSearch size={18} />
+    <Modal>
+      <Modal.Open opens='search_modal'>
+        <div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+          <div className='flex flex-row items-center justify-between'>
+            <div className='text-sm font-semibold px-6'>Anywhere</div>
+            <div className='hidden text-sm font-semibold px-6 border-x-[1px] flex-1 text-center sm:block'>
+              Any week
+            </div>
+            <div className='text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3'>
+              <div className='hidden sm:block'>Add Guests</div>
+              <div className='p-2 bg-rose-500 rounded-full text-white'>
+                <BiSearch size={18} />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Modal.Open>
+
+      <Modal.Window name='search_modal'>
+        <Modal.Heading>Filters</Modal.Heading>
+        <SearchModal />
+      </Modal.Window>
+    </Modal>
   );
 };
